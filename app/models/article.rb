c63897e0715bbde7baeab538a8a25dbf3e09ea5b
Ken Lam <hoangvu23108@gmail.com>
2016-06-20 21:57:34 +0700

    rollback query search form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   def self.search(search)
-    find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
-    # where('title ILIKE ?', "%#{search}%")
-    # where('body ILIKE ?', "%#{search}%")
+    # find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
+    where('title ILIKE ?', "%#{search}%")
+    where('body ILIKE ?', "%#{search}%")
   end
 end
